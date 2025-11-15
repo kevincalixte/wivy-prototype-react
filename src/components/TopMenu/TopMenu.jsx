89b0e07e5aedf670965d1aaaf774560056ab6a08
kevincalixte <kevin.calixte@icloud.com>
2025-11-15 01:56:51 +0100

    update

--- src/components/TopMenu/TopMenu.jsx
+++ src/components/TopMenu/TopMenu.jsx
@@ -8,7 +8,7 @@ function TopMenu() {
 
         <>
             <nav className='TopMenu bg-neutral-800 text-white w-full h-16 flex items-center justify-between px-5'>
-                <a href=""><img className='w-12' src='../../../public/assets/logos/wivy-app-logo-transparent.png' alt="Wivy Logo" /></a>
+                <a href=""><img className='w-12' src='/assets/logos/wivy-app-logo-transparent.png' alt="Wivy Logo" /></a>
                 <ul className='flex gap-10 items-center'>
                     <li className='flex gap-1 items-center hover:font-bold hover:scale-110 transform-gpu'><a href="" className='flex gap-1'><Search /><span>Meet</span></a></li>
                     <li className='flex gap-1 items-center hover:font-bold hover:text-blue-500 hover:scale-110 transform-gpu'><a href="" className='flex gap-1'><Users /><span>Friends</span></a></li>
