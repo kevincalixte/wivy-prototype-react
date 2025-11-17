61d409c7353b0985e84a0e5f539cba6a90a1f4ac
kevincalixte <kevin.calixte@icloud.com>
2025-11-17 02:24:36 +0100

    update

--- src/components/BorderShadow/BorderShadow.jsx
+++ src/components/BorderShadow/BorderShadow.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function BorderShadow({ color = "rgba(255,255,255,1)", children }) {
     return (
         <div
-            className="w-full h-20 -mt-20 flex items-start justify-center gap-5"
+            className="w-full h-20 -mt-20 flex items-start justify-center gap-10"
             style={{
                 background: `linear-gradient(rgba(0,0,0,0), ${color})`
             }}
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Button({ className = '', children }) {
     return (
         <div>
-            <button className={className}>
+            <button className={`hover:scale-120 transition-transorm duration-200 ${className}`}>
                 {children}
             </button>
         </div>
