f755cb4becbef3f8d25b687f100fd7aec0aa7b6d
kevincalixte <kevin.calixte@icloud.com>
2025-11-18 21:52:34 +0100

    update

--- src/components/FormSubscribe/FormSubscribe.jsx
+++ src/components/FormSubscribe/FormSubscribe.jsx
@@ -18,12 +18,13 @@ function FormSubscribe({setFormSubscribeVisible} ) {
     setFormSubscribeVisible(false);
   }
     return (
-        <div className='h-[90%] text-center  text-white text-lg bg-black opacity-55 rounded-2xl z-50 p-5'>
+        <div className='max-w-full h-[90%] text-center  text-white text-lg bg-black opacity-55 rounded-2xl z-50 p-5'>
+
 
             <h1 className='py-4 text-2xl font-bold'>We need your number 😉</h1>
             <div className='flex items-center justify-center gap-1'>
                 <Input className="w-30" label="Country" type="text" placeholder="FR +33" />
-                <Input label="Phone Number" type="text" placeholder="" />
+                <Input className= "w-50" label="Phone Number" type="text" placeholder="" />
             </div>
             <p className='text-sm text-left'>You will receive a text message to confirm your registration. Message and data usage charges may apply. <span className='text-emerald-300 hover:font-bold'><a href="">Have you lost your number?</a></span> </p>
             <div className='flex gap-10 justify-center '>
