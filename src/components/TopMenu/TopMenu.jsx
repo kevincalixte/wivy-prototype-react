8d7f625fc25076d8f298a96a074f5ed408f9f049
kevincalixte <kevin.calixte@icloud.com>
2025-11-20 11:13:50 +0100

    update

--- src/components/TopMenu/TopMenu.jsx
+++ src/components/TopMenu/TopMenu.jsx
@@ -26,7 +26,7 @@ function TopMenu({ setCurrentAlbum, meetImages, friendsImages, loveImages }) {
                     href=""
                     className={'animate-[slideFromRight_0.6s_ease-out]'}
                 >
-                    <img className='w-12' src='assets/logos/wivy-app-logo-transparent.png' alt="Wivy Logo" />
+                    <img className='w-12' src='/assets/logos/wivy-app-logo-transparent.png' alt="Wivy Logo" />
                 </a>
 
                 <ul className={`${showButtons ? 'flex' : 'hidden'} gap-2 items-center`}>
